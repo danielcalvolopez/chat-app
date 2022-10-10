994d788f8059fb144f08cd8911041d396378c18b
Daniel <acidistrict@gmail.com>
2022-10-10 20:41:58 +0100

    change color

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,14 @@
 import { useContext } from "react";
 import Chat from "../../components/Chat/Chat";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import { ChatContext } from "../../context/ChatContext";
 import classes from "./Home.module.css";
 
 const Home = () => {
-  const { data } = useContext(ChatContext);
-
   return (
     <div className={classes.home}>
       <div className={classes.container}>
         <Sidebar className={classes.sidebar} />
-        {data.chatId !== "null" && <Chat className={classes.chat} />}
+        <Chat className={classes.chat} />
       </div>
     </div>
   );
